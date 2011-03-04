416df417383f101f7d14cb70a5581edf1db110c4
Viresh Kumar <viresh.kumar@st.com>
2011-03-04 11:01:49 +0530

    Revert "ST SPEAr13xx DMAC: Using list_move instead of list_del + list_del"
    This reverts commit ab9ab19ccef66956f41254d9b58018d576eea733.

--- drivers/dma/dw_dmac.c
+++ drivers/dma/dw_dmac.c
@@ -84,6 +84,11 @@ static struct dw_desc *dwc_first_active(struct dw_dma_chan *dwc)
 	return list_entry(dwc->active_list.next, struct dw_desc, desc_node);
 }
 
+static struct dw_desc *dwc_first_queued(struct dw_dma_chan *dwc)
+{
+	return list_entry(dwc->queue.next, struct dw_desc, desc_node);
+}
+
 static struct dw_desc *dwc_desc_get(struct dw_dma_chan *dwc)
 {
 	struct dw_desc *desc, *_desc;
@@ -239,6 +244,7 @@ static void dwc_complete_all(struct dw_dma *dw, struct dw_dma_chan *dwc)
 {
 	struct dw_desc *desc, *_desc;
 	LIST_HEAD(list);
+	struct list_head *entry;
 
 	if (dma_readl(dw, CH_EN) & dwc->mask) {
 		dev_err(chan2dev(&dwc->chan),
@@ -250,12 +256,18 @@ static void dwc_complete_all(struct dw_dma *dw, struct dw_dma_chan *dwc)
 			cpu_relax();
 	}
 
+	/*
+	 * Submit queued descriptors ASAP, i.e. before we go through
+	 * the completed ones.
+	 */
 	list_splice_init(&dwc->active_list, &list);
 
 	/* remove only first desc from dwc->queue */
 	if (!list_empty(&dwc->queue)) {
-		list_move(dwc->queue.next, &dwc->active_list);
-		dwc_dostart(dwc, dwc_first_active(dwc));
+		dwc_dostart(dwc, dwc_first_queued(dwc));
+		entry = dwc->queue.next;
+		list_del(entry);
+		list_add(entry, &dwc->active_list);
 	}
 
 	list_for_each_entry_safe(desc, _desc, &list, desc_node)
@@ -313,8 +325,11 @@ static void dwc_scan_descriptors(struct dw_dma *dw, struct dw_dma_chan *dwc)
 		cpu_relax();
 
 	if (!list_empty(&dwc->queue)) {
-		list_move(dwc->queue.next, &dwc->active_list);
-		dwc_dostart(dwc, dwc_first_active(dwc));
+		struct list_head *entry;
+		dwc_dostart(dwc, dwc_first_queued(dwc));
+		entry = dwc->queue.next;
+		list_del(entry);
+		list_add(entry, &dwc->active_list);
 	}
 }
 
@@ -330,6 +345,7 @@ static void dwc_handle_error(struct dw_dma *dw, struct dw_dma_chan *dwc)
 {
 	struct dw_desc *bad_desc;
 	struct dw_desc *child;
+	struct list_head *entry;
 
 	dwc_scan_descriptors(dw, dwc);
 
@@ -340,7 +356,9 @@ static void dwc_handle_error(struct dw_dma *dw, struct dw_dma_chan *dwc)
 	 */
 	bad_desc = dwc_first_active(dwc);
 	list_del_init(&bad_desc->desc_node);
-	list_move(dwc->queue.next, dwc->active_list.prev);
+	entry = dwc->queue.next;
+	list_del(entry);
+	list_add(entry, dwc->active_list.prev);
 
 	/* Clear the error flag and try to restart the controller */
 	dma_writel(dw, CLEAR.ERROR, dwc->mask);
@@ -535,8 +553,8 @@ static dma_cookie_t dwc_tx_submit(struct dma_async_tx_descriptor *tx)
 	if (list_empty(&dwc->active_list)) {
 		dev_vdbg(chan2dev(tx->chan), "tx_submit: started %u\n",
 				desc->txd.cookie);
+		dwc_dostart(dwc, desc);
 		list_add_tail(&desc->desc_node, &dwc->active_list);
-		dwc_dostart(dwc, dwc_first_active(dwc));
 	} else {
 		dev_vdbg(chan2dev(tx->chan), "tx_submit: queued %u\n",
 				desc->txd.cookie);
