8338b7dec19b128f4762e887930fd4b72294488d
Viresh Kumar <viresh.kumar@st.com>
2011-05-13 15:15:03 +0530

    SPEAr: set spi max_speed_hz to 12 MHz
    Either we can have 21.5 MHz or 10.75 MHz. Max supported by spi memory device we
    have, M25P10, is 20 MHz. So, we chose 10.75. 12 MHz will be updated to 10.75
    automatically.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>

--- arch/arm/mach-spear3xx/spear300_evb.c
+++ arch/arm/mach-spear3xx/spear300_evb.c
@@ -155,7 +155,7 @@ static struct spi_board_info __initdata spi_board_info[] = {
 #endif
 		.modalias = "m25p80",
 		.controller_data = &spi0_flash_chip_info,
-		.max_speed_hz = 25000000,
+		.max_speed_hz = 12000000,
 		.bus_num = 0,
 		.chip_select = 1,
 		.mode = SPI_MODE_1,
