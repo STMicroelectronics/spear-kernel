88bb5fa7590f3a12e548bc4661cf004900815e7f
Rajeev Kumar <rajeev-dlh.kumar@st.com>
2011-05-25 16:53:29 +0530

    SPEAr13xx/keyboard: Pass mode information to keyboard driver
    Keyboard driver can support 9x9, 6x6 and 2x2 key matrix. Pass this(mode)
    information to driver from platform files. For spear1340 supported
    keyboard matrix are 6x6 and 2x2 only.
    
    Signed-off-by: Rajeev Kumar <rajeev-dlh.kumar@st.com>

--- arch/arm/mach-spear13xx/spear1310_evb.c
+++ arch/arm/mach-spear13xx/spear1310_evb.c
@@ -173,6 +173,7 @@ static struct matrix_keymap_data keymap_data = {
 static struct kbd_platform_data kbd_data = {
 	.keymap = &keymap_data,
 	.rep = 1,
+	.mode = KEYPAD_9x9,
 };
 
 /* spi master's configuration routine */
