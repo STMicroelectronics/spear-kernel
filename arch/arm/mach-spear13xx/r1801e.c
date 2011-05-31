1d6f6f7c79de399429ad06f523e3f2475001cb6a
Shiraz Hashim <shiraz.hashim@st.com>
2011-05-31 17:48:00 +0530

    spear1310/r1801e: Add support for ZT r1801e SOM board support
    ZT R1801E SOM board is based upon SPEAr1310 RevA device. Currently
    There is no documentation available for this board. This board supports
    only basic devices like Ethernet, NAND Flash, UART.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>

---
+++ arch/arm/mach-spear13xx/r1801e.c
@@ -0,0 +1,281 @@
+/*
+ * arch/arm/mach-spear13xx/r1801e.c
+ *
+ * ZT SOM board source file
+ *
+ * Copyright (C) 2010 ST Microelectronics
+ * Shiraz Hashim <shiraz.hashim@st.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <linux/clk.h>
+#include <linux/types.h>
+#include <linux/gpio.h>
+#include <linux/irq.h>
+#include <linux/mtd/fsmc.h>
+#include <linux/mtd/nand.h>
+#include <linux/phy.h>
+#include <linux/stmmac.h>
+#include <asm/mach-types.h>
+#include <plat/adc.h>
+#include <plat/fsmc.h>
+#include <plat/jpeg.h>
+#include <mach/generic.h>
+#include <mach/gpio.h>
+#include <mach/hardware.h>
+#include <mach/spear_pcie.h>
+
+#define PARTITION(n, off, sz)	{.name = n, .offset = off, .size = sz}
+
+#ifdef CONFIG_USE_PLGPIO
+/* Pad muxing for PLGPIO_11 "System Alive" LED indicator */
+static struct pmx_mux_reg pmx_plgpio_10_11_mux[] = {
+	{
+		.address = SPEAR13XX_PMX_CFG1,
+		.mask = SPEAR13XX_PMX_GPT0_TMR1_MASK,
+		.value = 0,
+	},
+};
+
+static struct pmx_dev_mode pmx_plgpio_10_11_modes[] = {
+	{
+		.mux_regs = pmx_plgpio_10_11_mux,
+		.mux_reg_cnt = ARRAY_SIZE(pmx_plgpio_10_11_mux),
+	},
+};
+
+struct pmx_dev pmx_plgpio_10_11 = {
+	.name = "plgpio_10_11",
+	.modes = pmx_plgpio_10_11_modes,
+	.mode_count = ARRAY_SIZE(pmx_plgpio_10_11_modes),
+};
+
+/* Pad muxing for PLGPIO_9 "PWR_OFF" output */
+static struct pmx_mux_reg pmx_plgpio_8_9_mux[] = {
+	{
+		.address = SPEAR13XX_PMX_CFG1,
+		.mask = SPEAR13XX_PMX_GPT0_TMR2_MASK,
+		.value = 0,
+	},
+};
+
+static struct pmx_dev_mode pmx_plgpio_8_9_modes[] = {
+	{
+		.mux_regs = pmx_plgpio_8_9_mux,
+		.mux_reg_cnt = ARRAY_SIZE(pmx_plgpio_8_9_mux),
+	},
+};
+
+static struct pmx_dev pmx_plgpio_8_9 = {
+	.name = "plgpio_8_9",
+	.modes = pmx_plgpio_8_9_modes,
+	.mode_count = ARRAY_SIZE(pmx_plgpio_8_9_modes),
+};
+
+/* Pad muxing for PLGPIO_7 "SHTDWN_RQST" input */
+static struct pmx_mux_reg pmx_plgpio_6_7_mux[] = {
+	{
+		.address = SPEAR13XX_PMX_CFG1,
+		.mask = SPEAR13XX_PMX_GPT1_TMR1_MASK,
+		.value = 0,
+	},
+};
+
+static struct pmx_dev_mode pmx_plgpio_6_7_modes[] = {
+	{
+		.mux_regs = pmx_plgpio_6_7_mux,
+		.mux_reg_cnt = ARRAY_SIZE(pmx_plgpio_6_7_mux),
+	},
+};
+
+static struct pmx_dev pmx_plgpio_6_7 = {
+	.name = "plgpio_6_7",
+	.modes = pmx_plgpio_6_7_modes,
+	.mode_count = ARRAY_SIZE(pmx_plgpio_6_7_modes),
+};
+#endif /* CONFIG_USE_PLGPIO */
+
+/* NAND partition table */
+static struct mtd_partition partition_info[] = {
+	PARTITION("X-loader", 0, 4 * 0x20000),
+	PARTITION("U-Boot", 0x80000, 3 * 0x20000),
+	PARTITION("Kernel", 0xe0000, 24 * 0x20000),
+	PARTITION("Root File System", 0x3e0000, 8161 * 0x20000),
+};
+
+/* Ethernet phy-0 device registeration */
+static struct plat_stmmacphy_data phy0_private_data = {
+	.bus_id = 0,
+	.phy_addr = 0,
+	.phy_mask = 0,
+	.interface = PHY_INTERFACE_MODE_GMII,
+	.phy_clk_cfg = spear13xx_eth_phy_clk_cfg,
+};
+
+static struct resource phy0_resources = {
+	.name = "phyirq",
+	.start = -1,
+	.end = -1,
+	.flags = IORESOURCE_IRQ,
+};
+
+static struct platform_device r1801e_phy0_device = {
+	.name		= "stmmacphy",
+	.id		= 0,
+	.num_resources	= 1,
+	.resource	= &phy0_resources,
+	.dev.platform_data = &phy0_private_data,
+};
+
+/* padmux devices to enable */
+static struct pmx_dev *pmx_devs[] = {
+	/* spear13xx specific devices */
+	&spear13xx_pmx_i2c,
+	&spear13xx_pmx_egpio_grp,
+	&spear13xx_pmx_gmii,
+	&spear13xx_pmx_mcif,
+	&spear13xx_pmx_uart0,
+	&spear13xx_pmx_nand_16bit,
+
+	/* spear1310 specific devices */
+	&spear1310_reva_pmx_i2c1,
+	&spear1310_reva_pmx_uart_1,
+	&spear1310_reva_pmx_uart_2,
+	&spear1310_reva_pmx_uart_3_4_5,
+#ifdef CONFIG_USE_PLGPIO
+	&pmx_plgpio_10_11,
+	&pmx_plgpio_8_9,
+	&pmx_plgpio_6_7,
+#endif
+};
+
+static struct amba_device *amba_devs[] __initdata = {
+	/* spear13xx specific devices */
+	&spear13xx_gpio_device[0],
+	&spear13xx_gpio_device[1],
+	&spear13xx_ssp_device,
+	&spear13xx_uart_device,
+
+	/* spear1310 specific devices */
+	&spear1310_uart1_device,
+	&spear1310_uart2_device,
+	&spear1310_uart3_device,
+	&spear1310_uart4_device,
+	&spear1310_uart5_device,
+};
+
+static struct platform_device *plat_devs[] __initdata = {
+	/* spear13xx specific devices */
+	&spear13xx_adc_device,
+	&spear13xx_dmac_device[0],
+	&spear13xx_dmac_device[1],
+	&spear13xx_ehci0_device,
+	&spear13xx_ehci1_device,
+	&spear13xx_eth_device,
+	&spear13xx_i2c_device,
+	&spear13xx_jpeg_device,
+	&spear13xx_ohci0_device,
+	&spear13xx_ohci1_device,
+	&spear13xx_pcie_host0_device,
+	&spear13xx_pcie_host1_device,
+	&spear13xx_pcie_host2_device,
+	&spear13xx_rtc_device,
+	&spear13xx_sdhci_device,
+	&spear13xx_wdt_device,
+	&spear13xx_nand_device,
+
+	/* spear1310 specific devices */
+	&spear1310_reva_i2c1_device,
+	&spear1310_reva_plgpio_device,
+	&r1801e_phy0_device,
+};
+
+#ifdef CONFIG_SPEAR_PCIE_REV341
+ /* This function is needed for board specific PCIe initilization */
+static void __init r1801e_pcie_board_init(void)
+{
+	void *plat_data;
+
+	plat_data = dev_get_platdata(&spear13xx_pcie_host0_device.dev);
+	PCIE_PORT_INIT((struct pcie_port_info *)plat_data, SPEAR_PCIE_REV_3_41);
+
+	plat_data = dev_get_platdata(&spear13xx_pcie_host1_device.dev);
+	PCIE_PORT_INIT((struct pcie_port_info *)plat_data, SPEAR_PCIE_REV_3_41);
+
+	plat_data = dev_get_platdata(&spear13xx_pcie_host2_device.dev);
+	PCIE_PORT_INIT((struct pcie_port_info *)plat_data, SPEAR_PCIE_REV_3_41);
+}
+#endif
+
+static void __init ras_fsmc_config(u32 mode, u32 width)
+{
+	u32 val, *address;
+
+	address = ioremap(SPEAR1310_RAS_CTRL_REG0, SZ_16);
+
+	val = readl(address);
+	val &= ~(RAS_FSMC_MODE_MASK | RAS_FSMC_WIDTH_MASK);
+	val |= mode;
+	val |= width;
+	val |= RAS_FSMC_CS_SPLIT;
+
+	writel(val, address);
+
+	iounmap(address);
+}
+
+static void __init r1801e_init(void)
+{
+	int i;
+
+	/* set adc platform data */
+	set_adc_plat_data(&spear13xx_adc_device, &spear13xx_dmac_device[0].dev);
+
+	/* set jpeg configurations for DMA xfers */
+	set_jpeg_dma_configuration(&spear13xx_jpeg_device,
+			&spear13xx_dmac_device[0].dev);
+
+	/*
+	 * SPEAr1310 FSMC cannot used as NOR and NAND at the same time
+	 * For the moment, disable NAND and use NOR only
+	 * If NAND is needed, enable the following code and disable all
+	 * code for NOR. Also enable nand in padmux configuration to
+	 * use it.
+	 */
+	/* set nand device's plat data */
+	fsmc_nand_set_plat_data(&spear13xx_nand_device, partition_info,
+			ARRAY_SIZE(partition_info), NAND_SKIP_BBTSCAN,
+			FSMC_NAND_BW16);
+	nand_mach_init(FSMC_NAND_BW16);
+
+	/* call spear1310 machine init function */
+	spear1310_init(NULL, pmx_devs, ARRAY_SIZE(pmx_devs));
+
+#ifdef CONFIG_SPEAR_PCIE_REV341
+	/* Enable PCIE0 clk */
+	enable_pcie0_clk();
+	r1801e_pcie_board_init();
+#endif
+
+	/* Add Platform Devices */
+	platform_add_devices(plat_devs, ARRAY_SIZE(plat_devs));
+
+	/* Add Amba Devices */
+	for (i = 0; i < ARRAY_SIZE(amba_devs); i++)
+		amba_device_register(amba_devs[i], &iomem_resource);
+
+	/* Initialize fsmc regiters */
+	ras_fsmc_config(RAS_FSMC_MODE_NAND, RAS_FSMC_WIDTH_16);
+
+}
+
+MACHINE_START(R1801E, "ST-SPEAR1310-REVA-R1801e")
+	.boot_params	=	0x00000100,
+	.map_io		=	spear13xx_map_io,
+	.init_irq	=	spear13xx_init_irq,
+	.timer		=	&spear13xx_timer,
+	.init_machine	=	r1801e_init,
+MACHINE_END
