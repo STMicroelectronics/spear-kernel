3ed7dbb9d04ec7a37c71b9d76b6a9f8fcca18e34
Shiraz Hashim <shiraz.hashim@st.com>
2011-06-13 13:48:34 +0530

    spear/static map: correct SRAM1 size to 4KB rather than 1MB
    Correct SRAM1 size to actual 4KB, which was wrongly defined as 1MB.
    This also changes static mapping related to SRAM1 region.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>

--- arch/arm/mach-spear13xx/spear13xx.c
+++ arch/arm/mach-spear13xx/spear13xx.c
@@ -1239,7 +1239,7 @@ struct map_desc spear13xx_io_desc[] __initdata = {
 	}, {
 		.virtual	= IO_ADDRESS(SPEAR13XX_SYSRAM1_BASE),
 		.pfn		= __phys_to_pfn(SPEAR13XX_SYSRAM1_BASE),
-		.length		= SZ_1M,
+		.length		= SZ_4K,
 		.type		= MT_MEMORY_NONCACHED
 	}, {
 		.virtual	= IO_ADDRESS(SPEAR13XX_PCIE0_BASE),
