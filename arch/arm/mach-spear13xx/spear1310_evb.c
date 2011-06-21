1f6d8afcf1c601cacdd537d1e32a31c924d88a14
Pratyush Anand <pratyush.anand@st.com>
2011-06-21 11:53:01 +0530

    SPEAr1310 evb: plat device entry for OTG added
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- arch/arm/mach-spear13xx/spear1310_evb.c
+++ arch/arm/mach-spear13xx/spear1310_evb.c
@@ -160,6 +160,7 @@ static struct platform_device *plat_devs[] __initdata = {
 	&spear1310_tdm_hdlc_1_device,
 	&spear1310_rs485_0_device,
 	&spear1310_rs485_1_device,
+	&spear1310_otg_device,
 };
 
 static struct arasan_cf_pdata cf_pdata = {
