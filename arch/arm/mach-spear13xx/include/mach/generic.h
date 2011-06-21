1a1ba16983eec4e402e56cc42f14b13b7d88f6b0
Pratyush Anand <pratyush.anand@st.com>
2011-06-21 11:53:00 +0530

    SPEAr1310: Platform definition for OTG added
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- arch/arm/mach-spear13xx/include/mach/generic.h
+++ arch/arm/mach-spear13xx/include/mach/generic.h
@@ -570,6 +570,7 @@ extern struct platform_device spear1310_tdm_hdlc_0_device;
 extern struct platform_device spear1310_tdm_hdlc_1_device;
 extern struct platform_device spear1310_rs485_0_device;
 extern struct platform_device spear1310_rs485_1_device;
+extern struct platform_device spear1310_otg_device;
 
 /* Add spear1310 machine function declarations here */
 void __init spear1310_clk_init(void);
