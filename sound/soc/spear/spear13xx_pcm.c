26d2245f4b838e200cab83dbd27fe8d0af64ccd6
Shiraz Hashim <shiraz.hashim@st.com>
2011-06-29 11:11:16 +0530

    sound/asoc/spear: fix incorrect DMA I2S Tx line assignment
    By mistake I2C Tx line was associated with the DMA. Fix this by
    assigning correct Tx line for I2S.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>

--- sound/soc/spear/spear13xx_pcm.c
+++ sound/soc/spear/spear13xx_pcm.c
@@ -57,7 +57,7 @@ void pcm_init(struct device *dma_dev)
 	data.i2s2mem_slave.dma_dev = dma_dev;
 	/* doing 16 bit audio transfer */
 	data.mem2i2s_slave.reg_width = DW_DMA_SLAVE_WIDTH_16BIT;
-	data.mem2i2s_slave.cfg_hi = DWC_CFGH_DST_PER(SPEAR13XX_DMA_REQ_I2C_TX);
+	data.mem2i2s_slave.cfg_hi = DWC_CFGH_DST_PER(SPEAR13XX_DMA_REQ_I2S_TX);
 	data.mem2i2s_slave.cfg_lo = 0;
 	data.mem2i2s_slave.src_master = 1;
 	data.mem2i2s_slave.dst_master = 1;
