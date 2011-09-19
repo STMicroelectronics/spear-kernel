49bcd53df24a6b079982848a2a8ecb82cf1d4917
Rajeev Kumar <rajeev-dlh.kumar@st.com>
2011-09-19 11:19:00 +0530

    spear/crypto:hamcsha1zerolenkey:Insert check for zero len key.
    Placing a check for zero len key in case of  hmac.hamcsha1zerolenkey.
    Return C3_ERR if key len is zero.
    
    Signed-off-by: Rajeev Kumar <rajeev-dlh.kumar@st.com>

--- drivers/char/c3/c3_driver_interface.c
+++ drivers/char/c3/c3_driver_interface.c
@@ -2743,26 +2743,33 @@ unsigned int c3_SHA1_HMAC_init(
 	struct c3_dma_t *c3_dma_0 = NULL;
 	struct c3_dma_t *c3_dma_1 = NULL;
 
-	C3_START(UH_CHANNEL_INFO.ids_idx, prgmem, callback, callback_param);
+	if (!key_len) {
+		printk(C3_KERN_ERR "Invalid key\n");
+		return C3_ERR;
+	}
+	{
 
-	C3_PREPARE_DMA(c3_dma_0, ctx, C3_DRIVER_DIGEST_CTX_MAX_SIZE,
-		       DMA_FROM_DEVICE);
-	C3_PREPARE_DMA(c3_dma_1, key, key_len, DMA_TO_DEVICE);
+		C3_START(UH_CHANNEL_INFO.ids_idx, prgmem, callback,
+				callback_param);
 
-	SHA1_HMAC_INIT(
-		key_len,
-		c3_dma_1->dma_addr,
-		prgmem);
+		C3_PREPARE_DMA(c3_dma_0, ctx, C3_DRIVER_DIGEST_CTX_MAX_SIZE,
+				DMA_FROM_DEVICE);
+		C3_PREPARE_DMA(c3_dma_1, key, key_len, DMA_TO_DEVICE);
 
-	SAVE_CONTEXT(
-		c3_dma_0->dma_addr,
-		UH_CHANNEL_INFO.channel_idx,
-		prgmem);
+		SHA1_HMAC_INIT(
+				key_len,
+				c3_dma_1->dma_addr,
+				prgmem);
 
-	STOP(prgmem);
+		SAVE_CONTEXT(
+				c3_dma_0->dma_addr,
+				UH_CHANNEL_INFO.channel_idx,
+				prgmem);
 
-	C3_END(prgmem);
+		STOP(prgmem);
 
+		C3_END(prgmem);
+	}
 #else
 
 	return C3_ERR;
