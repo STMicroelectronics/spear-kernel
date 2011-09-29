c44ade6fcab01a07e94d450537caa32cc28a8218
Shiraz Hashim <shiraz.hashim@st.com>
2011-09-29 22:30:41 +0530

    SPEAr1340/cpufreq: Include support for cpu freq scaling till 600 MHz
    VCO1 feeds (through VCO1div2) several synthesizer (with an upper
    limit of 500 MHz at the source). Hence VCO1 cannot go beyond 1000 MHz.
    
    PLL1 being equal to VCO1/2^p cannot be greater than 1000 MHz resulting
    into CPU (being fed through PLL1  ==> sys_clk == /2 ==> cpu) not more
    than 500 MHz.
    
    To run cpu at >= 500 MHz (till 600 MHz) we need to arrange for another
    PLL (PLL2) to feed asynchronously to cpu.
    
    SPEAr1340 sys_clk can have multiple clock sources, namely
       - sys synthesizer
       - pll1
       - pll2
       - pll3
    
    In this patch we choose and configure PLL2 as the clock source for sys
    clk when cpu is to be fed > 250 MHz and switch to sys synthesizer when
    required cpu clock <= 250 MHz.
    
    TODO: Call clk_disable for clk src which is not in use now.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>

--- arch/arm/plat-spear/cpufreq.c
+++ arch/arm/plat-spear/cpufreq.c
@@ -24,32 +24,52 @@
 #include <asm/system.h>
 #include <mach/hardware.h>
 
-#define CPU_CLK		"cpu_clk"
+#define CPU_CLK_MAX_STEP	10
 
 #ifdef CONFIG_ARCH_SPEAR13XX
-#define MIN_CPU_FREQ	200000
-#define MAX_CPU_FREQ	500000
+#if defined(CONFIG_CPU_SPEAR1300) || defined(CONFIG_CPU_SPEAR1310_REVA) || \
+	defined (CONFIG_CPU_SPEAR900)
+#define SPEAR13XX_MIN_CPU_FREQ	200000
+#define SPEAR13XX_MAX_CPU_FREQ	500000
 
-static u32 spear_cpu_freq[] = {
+static u32 spear13xx_cpu_freq[] = {
 	200000, /* 200 MHZ */
 	250000, /* 250 MHZ */
 	332000, /* 332 MHZ */
 	400000, /* 400 MHZ */
 	500000, /* 500 MHZ */
 };
-#elif defined(CONFIG_ARCH_SPEAR6XX) || defined(CONFIG_ARCH_SPEAR3XX)
+#endif /* CONFIG_ARCH_SPEAR13XX and !CONFIG_CPU_SPEAR1340 */
+
+#ifdef CONFIG_CPU_SPEAR1340
+#define SPEAR1340_MIN_CPU_FREQ	166000
+#define SPEAR1340_MAX_CPU_FREQ	600000
+
+static u32 spear1340_cpu_freq[] = {
+	166000, /* 166 MHZ */
+	200000, /* 200 MHZ */
+	250000, /* 250 MHZ */
+	332000, /* 332 MHZ */
+	400000, /* 400 MHZ */
+	500000, /* 500 MHZ */
+	600000, /* 600 MHZ */
+};
+
+#endif /* CONFIG_CPU_SPEAR1340 */
+#endif /* CONFIG_ARCH_SPEAR13XX */
+
+#if defined(CONFIG_ARCH_SPEAR6XX) || defined(CONFIG_ARCH_SPEAR3XX)
 #define MIN_CPU_FREQ	166000
 #define MAX_CPU_FREQ	332000
 
 static u32 spear_cpu_freq[] = {
 	166000, /* 166 MHZ */
 	266000, /* 266 MHZ */
-	332000, /* 333 MHZ */
+	332000, /* 332 MHZ */
 };
 #endif
 
-static struct
-	cpufreq_frequency_table spear_freq_tbl[ARRAY_SIZE(spear_cpu_freq) + 1];
+static struct cpufreq_frequency_table spear_freq_tbl[CPU_CLK_MAX_STEP];
 static struct clk *cpu_clk;
 
 int spear_cpufreq_verify(struct cpufreq_policy *policy)
@@ -62,12 +82,80 @@ static unsigned int spear_cpufreq_get(unsigned int cpu)
 	return cpu ? 0 : clk_get_rate(cpu_clk) / 1000;
 }
 
+static struct clk *spear1340_cpu_get_possible_parent(unsigned long newfreq)
+{
+	int pclk;
+	struct clk *sys_pclk;
+	/*
+	 * In SPEAr1340, cpu clk's parent sys clk can take input from
+	 * following sources
+	 */
+	const char *sys_clk_src[] = {
+		"sys_synth_clk",
+		"pll1_clk",
+		"pll2_clk",
+		"pll3_clk",
+	};
+
+	/*
+	 * As sys clk can have multiple source with their own range
+	 * limitation so we choose possible sources accordingly
+	 */
+	if (newfreq <= 250000000)
+		pclk = 0; /* src is sys_synth_clk */
+	else if (newfreq <= 600000000)
+		pclk = 2; /* src is pll2_clk */
+	else
+		return ERR_PTR(-EINVAL);
+
+	/* Get parent to sys clock */
+	sys_pclk = clk_get(NULL, sys_clk_src[pclk]);
+	if (IS_ERR(sys_pclk))
+		pr_err("SPEAr1340: Failed to get %s clock\n",
+				sys_clk_src[pclk]);
+
+	return sys_pclk;
+}
+
+static int spear1340_set_cpu_rate(struct clk *cpu_clk, struct clk *sys_pclk,
+		unsigned long newfreq)
+{
+	struct clk *sys_clk;
+	int ret = 0;
+
+	sys_clk = clk_get_parent(cpu_clk);
+	if (IS_ERR(sys_clk)) {
+		pr_err("failed to get cpu's parent (sys) clock\n");
+		return PTR_ERR(sys_clk);
+	}
+
+	/*
+	 * Set the rate of the source clock before changing the parent
+	 * Note: newfreq = intended cpu_clk * 2 in case of SPEAr1340
+	 */
+	ret = clk_set_rate(sys_pclk, newfreq);
+	if (ret) {
+		pr_err("SPEAr1340: Failed to set sys clk rate to %lu\n",
+				newfreq);
+		return ret;
+	}
+
+	ret = clk_set_parent(sys_clk, sys_pclk);
+	if (ret) {
+		pr_err("SPEAr1340: Failed to set sys clk parent\n");
+		return ret;
+	}
+
+	return 0;
+}
+
 static int spear_cpufreq_target(struct cpufreq_policy *policy,
 		unsigned int target_freq, unsigned int relation)
 {
 	struct cpufreq_freqs freqs;
 	int index, ret;
-	long newfreq;
+	unsigned long newfreq, srcfreq;
+	struct clk *srcclk;
 
 	if (policy->cpu != 0)
 		return -EINVAL;
@@ -82,18 +170,56 @@ static int spear_cpufreq_target(struct cpufreq_policy *policy,
 	if (freqs.old == target_freq)
 		return 0;
 
-	newfreq = clk_round_rate(cpu_clk, spear_cpu_freq[index] * 1000);
-	if (newfreq < 0) {
-		pr_err("CPU Freq: clk_round_rate failed: %ld\n", newfreq);
-		return newfreq;
+	newfreq = spear_freq_tbl[index].frequency * 1000;
+	if (cpu_is_spear1340()) {
+		/*
+		 * SPEAr1340 is special in the sense that due to the
+		 * possibility of multiple clock sources for cpu clk's
+		 * parent we can have different clock source for
+		 * different frequency of cpu clk.
+		 * Hence we need to choose one from amongst these
+		 * possible clock sources.
+		 */
+		srcclk = spear1340_cpu_get_possible_parent(newfreq);
+		if (IS_ERR(srcclk)) {
+			pr_err("Failed to get src clk\n");
+			return PTR_ERR(srcclk);
+		}
+
+		/* SPEAr1340: src clk is always 2 * intended cpu clk */
+		srcfreq = newfreq * 2;
+	} else {
+		/*
+		 * Rest: src clock to be altered is ancestor of cpu
+		 * clock. Hence we can directly work on cpu clk
+		 */
+		srcclk = cpu_clk;
+		srcfreq = newfreq;
 	}
 
-	freqs.new = newfreq / 1000;
+	/*
+	 * In SPEAr1340, we cannot use newfreq directly because we need
+	 * to actually access a source clock (clk) which might not be
+	 * ancestor of cpu at present.
+	 * Hence in SPEAr1340 we would operate on source clock directly
+	 * before switching cpu clock to it.
+	 */
+	srcfreq = clk_round_rate(srcclk, srcfreq);
+	if (srcfreq < 0) {
+		pr_err("CPU Freq: clk_round_rate failed for cpu src clock\n");
+		return srcfreq;
+	}
 
+	freqs.new = srcfreq / 1000;
+	freqs.new /= cpu_is_spear1340() ? 2 : 1;
 	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
-	/* Get current rate after clk_set_rate, for both success and failure */
-	ret = clk_set_rate(cpu_clk, freqs.new * 1000);
+	if (cpu_is_spear1340())
+		ret = spear1340_set_cpu_rate(cpu_clk, srcclk, srcfreq);
+	else
+		ret = clk_set_rate(cpu_clk, srcfreq);
+
+	/* Get current rate after clk_set_rate, in case of failure */
 	if (ret) {
 		pr_err("CPU Freq: cpu clk_set_rate failed: %d\n", ret);
 		freqs.new = clk_get_rate(cpu_clk) / 1000;
@@ -105,22 +231,47 @@ static int spear_cpufreq_target(struct cpufreq_policy *policy,
 
 static int spear_cpufreq_init(struct cpufreq_policy *policy)
 {
-	int i = 0;
+	int i = 0, table_len = 0;
+	u32 *cpu_freq_table;
 
 	if (policy->cpu != 0)
 		return -EINVAL;
 
-	cpu_clk = clk_get(NULL, CPU_CLK);
+	cpu_clk = clk_get(NULL, "cpu_clk");
 	if (IS_ERR(cpu_clk))
 		return PTR_ERR(cpu_clk);
 
-	policy->cpuinfo.min_freq = MIN_CPU_FREQ;
-	policy->cpuinfo.max_freq = MAX_CPU_FREQ;
+	if (cpu_is_spear1340()) {
+#ifdef CONFIG_CPU_SPEAR1340
+		policy->cpuinfo.min_freq = SPEAR1340_MIN_CPU_FREQ;
+		policy->cpuinfo.max_freq = SPEAR1340_MAX_CPU_FREQ;
+		cpu_freq_table = spear1340_cpu_freq;
+		table_len = ARRAY_SIZE(spear1340_cpu_freq);
+#endif
+	} else if (arch_is_spear13xx()) {
+#ifdef CONFIG_ARCH_SPEAR13XX
+		policy->cpuinfo.min_freq = SPEAR13XX_MIN_CPU_FREQ;
+		policy->cpuinfo.max_freq = SPEAR13XX_MAX_CPU_FREQ;
+		cpu_freq_table = spear13xx_cpu_freq;
+		table_len = ARRAY_SIZE(spear13xx_cpu_freq);
+#endif
+	} else if (arch_is_spear3xx() || arch_is_spear6xx()) {
+#if defined(CONFIG_ARCH_SPEAR3XX) || defined(CONFIG_ARCH_SPEAR6XX)
+		policy->cpuinfo.min_freq = MIN_CPU_FREQ;
+		policy->cpuinfo.max_freq = MAX_CPU_FREQ;
+		cpu_freq_table = spear_cpu_freq;
+		table_len = ARRAY_SIZE(spear_cpu_freq);
+#endif
+	} else {
+		pr_err("Error: No valid cpu found\n");
+		return -EINVAL;
+	}
+
 	policy->cur = policy->min = policy->max = spear_cpufreq_get(0);
 
-	for (i = 0; i < ARRAY_SIZE(spear_cpu_freq); i++) {
+	for (i = 0; i < table_len; i++) {
 		spear_freq_tbl[i].index = i;
-		spear_freq_tbl[i].frequency = spear_cpu_freq[i];
+		spear_freq_tbl[i].frequency = cpu_freq_table[i];
 	}
 
 	spear_freq_tbl[i].index = i;
