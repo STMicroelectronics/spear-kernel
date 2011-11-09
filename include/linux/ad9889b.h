1e96835c822959bb53cf9e8abb652216314af058
Pratyush Anand <pratyush.anand@st.com>
2011-11-09 12:37:38 +0530

    video/display: Add audio input selection for ad9889b HDMI driver
    Currently only SPDIF can be selected as audio input for AD9889B HDMI TX
    driver. A platform enum has been provided in this patch to select audio
    input between SPDIF and I2S.
    
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- include/linux/ad9889b.h
+++ include/linux/ad9889b.h
@@ -3,10 +3,16 @@
 
 #include <linux/fb.h>
 
+enum hdmi_audio_input {
+	HDMI_AUDIO_IN_SPDIF,
+	HDMI_AUDIO_IN_I2S,
+};
+
 struct ad9889b_pdata {
 	u32 irq_gpio;
 	u32 irq_type;
 	int fb;
+	enum hdmi_audio_input ain;
 };
 #endif /* __AD9889B_H */
 
