d7014b27d751e0562b936a66d8bf5245efc032e2
Pratyush Anand <pratyush.anand@st.com>
2011-11-09 12:37:38 +0530

    video/display: Add audio input selection for ad9889b HDMI driver
    Currently only SPDIF can be selected as audio input for AD9889B HDMI TX
    driver. A platform enum has been provided in this patch to select audio
    input between SPDIF and I2S.
    
    Signed-off-by: Pratyush Anand <pratyush.anand@st.com>

--- drivers/video/display/ad9889b/ad9889b.c
+++ drivers/video/display/ad9889b/ad9889b.c
@@ -553,6 +553,8 @@ static void ad9889b_audio_setup(struct i2c_client *client)
 /* Configure hdmi transmitter. */
 static void ad9889b_setup(struct i2c_client *client)
 {
+	struct ad9889b_pdata *pdata = dev_get_platdata(&client->dev);
+
 	/* Input format: RGB 4:4:4 */
 	ad9889b_wr_and_or(client, 0x15, 0xf1, 0x0);
 	/* Output format: RGB 4:4:4 */
@@ -582,7 +584,10 @@ static void ad9889b_setup(struct i2c_client *client)
 	/* Positive clk edge capture for input video clock */
 	ad9889b_wr_and_or(client, 0xba, 0x1f, 0x60);
 
-	ad9889b_wr(client, 0x44, 0xF8);
+	if (pdata->ain == HDMI_AUDIO_IN_SPDIF)
+		ad9889b_wr(client, 0x44, 0xF8);
+	else
+		ad9889b_wr(client, 0x44, 0x78);
 	ad9889b_wr_and_or(client, 0x73, 0xf8, 0x01);
 
 	ad9889b_audio_setup(client);
@@ -594,6 +599,7 @@ static void ad9889b_setup(struct i2c_client *client)
 static int ad9889b_s_power(struct i2c_client *client, u8 on)
 {
 	struct ad9889b_state *state = i2c_get_clientdata(client);
+	struct ad9889b_pdata *pdata = dev_get_platdata(&client->dev);
 	const int retries = 20;
 	int i;
 
@@ -645,7 +651,10 @@ static int ad9889b_s_power(struct i2c_client *client, u8 on)
 	ad9889b_wr(client, 0xa3, 0x84);
 	/* pixel clock > 80 then 87 otherwise 84 */
 	udelay(10); /* adding udelays between successive writes */
-	ad9889b_wr_and_or(client, 0x0a, 0x9f, 0x10);
+	if (pdata->ain == HDMI_AUDIO_IN_SPDIF)
+		ad9889b_wr_and_or(client, 0x0a, 0x9f, 0x10);
+	else
+		ad9889b_wr_and_or(client, 0x0a, 0x9f, 0x00);
 	udelay(10); /* adding udelays between successive writes */
 	ad9889b_wr(client, 0x9f, 0x70);
 	/* according to document it shud be 00 for proper operation */
