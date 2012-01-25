8c3dcb460cf4b19ce8810f26af22cd0d78690315
Vipin Kumar <vipin.kumar@st.com>
2012-01-25 15:31:56 +0530

    spdif out: Add support for power management to spdif out
    This patch enables support for sleep/wakeup and hibernate/restore functionality
    for spdif out driver
    
    Signed-off-by: Vipin Kumar <vipin.kumar@st.com>

--- sound/soc/spear/spdif_out.c
+++ sound/soc/spear/spdif_out.c
@@ -22,11 +22,34 @@
 #include <mach/spdif.h>
 #include "spdif_out_regs.h"
 
+struct spdif_out_params {
+	u32 rate;
+	u32 core_freq;
+};
+
 struct spdif_out_dev {
 	struct clk *clk;
 	struct dma_data dma_params;
+	struct spdif_out_params saved_params;
+	u32 running;
 	void *io_base;
 };
+
+static void spdif_out_configure(struct spdif_out_dev *host)
+{
+	writel(SPDIF_OUT_RESET, host->io_base + SPDIF_OUT_SOFT_RST);
+	mdelay(1);
+	writel(readl(host->io_base + SPDIF_OUT_SOFT_RST) & ~SPDIF_OUT_RESET,
+			host->io_base + SPDIF_OUT_SOFT_RST);
+
+	writel(SPDIF_OUT_FDMA_TRIG_16 | SPDIF_OUT_MEMFMT_16_16 |
+			SPDIF_OUT_VALID_HW | SPDIF_OUT_USER_HW |
+			SPDIF_OUT_CHNLSTA_HW | SPDIF_OUT_PARITY_HW,
+			host->io_base + SPDIF_OUT_CFG);
+
+	writel(0x7F, host->io_base + SPDIF_OUT_INT_STA_CLR);
+	writel(0x7F, host->io_base + SPDIF_OUT_INT_EN_CLR);
+}
 
 static int spdif_out_startup(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *cpu_dai)
@@ -43,18 +66,8 @@ static int spdif_out_startup(struct snd_pcm_substream *substream,
 	if (ret)
 		return ret;
 
-	writel(SPDIF_OUT_RESET, host->io_base + SPDIF_OUT_SOFT_RST);
-	msleep(1);
-	writel(readl(host->io_base + SPDIF_OUT_SOFT_RST) & ~SPDIF_OUT_RESET,
-			host->io_base + SPDIF_OUT_SOFT_RST);
-
-	writel(SPDIF_OUT_FDMA_TRIG_16 | SPDIF_OUT_MEMFMT_16_16 |
-			SPDIF_OUT_VALID_HW | SPDIF_OUT_USER_HW |
-			SPDIF_OUT_CHNLSTA_HW | SPDIF_OUT_PARITY_HW,
-			host->io_base + SPDIF_OUT_CFG);
-
-	writel(0x7F, host->io_base + SPDIF_OUT_INT_STA_CLR);
-	writel(0x7F, host->io_base + SPDIF_OUT_INT_EN_CLR);
+	host->running = true;
+	spdif_out_configure(host);
 
 	return 0;
 }
@@ -68,7 +81,22 @@ static void spdif_out_shutdown(struct snd_pcm_substream *substream,
 		return;
 
 	clk_disable(host->clk);
+	host->running = false;
 	snd_soc_dai_set_dma_data(dai, substream, NULL);
+}
+
+static void spdif_out_clock(struct spdif_out_dev *host, u32 core_freq,
+		u32 rate)
+{
+	u32 divider, ctrl;
+
+	clk_set_rate(host->clk, core_freq);
+	divider = DIV_ROUND_CLOSEST(clk_get_rate(host->clk), (rate * 128));
+
+	ctrl = readl(host->io_base + SPDIF_OUT_CTRL);
+	ctrl &= ~SPDIF_DIVIDER_MASK;
+	ctrl |= (divider << SPDIF_DIVIDER_SHIFT) & SPDIF_DIVIDER_MASK;
+	writel(ctrl, host->io_base + SPDIF_OUT_CTRL);
 }
 
 static int spdif_out_hw_params(struct snd_pcm_substream *substream,
@@ -76,7 +104,7 @@ static int spdif_out_hw_params(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *dai)
 {
 	struct spdif_out_dev *host = snd_soc_dai_get_drvdata(dai);
-	u32 rate, ctrl, divider, spdif_core_freq;
+	u32 rate, core_freq;
 
 	if (substream->stream != SNDRV_PCM_STREAM_PLAYBACK)
 		return -EINVAL;
@@ -92,7 +120,7 @@ static int spdif_out_hw_params(struct snd_pcm_substream *substream,
 		 * The clock is multiplied by 10 to bring it to feasible range
 		 * of frequencies for sscg
 		 */
-		spdif_core_freq = 64000 * 128 * 10;	/* 81.92 MHz */
+		core_freq = 64000 * 128 * 10;	/* 81.92 MHz */
 		break;
 	case 5512:
 	case 11025:
@@ -100,23 +128,19 @@ static int spdif_out_hw_params(struct snd_pcm_substream *substream,
 	case 44100:
 	case 88200:
 	case 176400:
-		spdif_core_freq = 176400 * 128;	/* 22.5792 MHz */
+		core_freq = 176400 * 128;	/* 22.5792 MHz */
 		break;
 	case 48000:
 	case 96000:
 	case 192000:
 	default:
-		spdif_core_freq = 192000 * 128;	/* 24.576 MHz */
+		core_freq = 192000 * 128;	/* 24.576 MHz */
 		break;
 	}
 
-	clk_set_rate(host->clk, spdif_core_freq);
-	divider = DIV_ROUND_CLOSEST(clk_get_rate(host->clk), (rate * 128));
-
-	ctrl = readl(host->io_base + SPDIF_OUT_CTRL);
-	ctrl &= ~SPDIF_DIVIDER_MASK;
-	ctrl |= (divider << SPDIF_DIVIDER_SHIFT) & SPDIF_DIVIDER_MASK;
-	writel(ctrl, host->io_base + SPDIF_OUT_CTRL);
+	spdif_out_clock(host, core_freq, rate);
+	host->saved_params.core_freq = core_freq;
+	host->saved_params.rate = rate;
 
 	return 0;
 }
@@ -238,12 +262,49 @@ static int spdif_out_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_PM
+static int spdif_out_suspend(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct spdif_out_dev *host = dev_get_drvdata(&pdev->dev);
+
+	if (host->running)
+		clk_disable(host->clk);
+
+	return 0;
+}
+
+static int spdif_out_resume(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct spdif_out_dev *host = dev_get_drvdata(&pdev->dev);
+
+	if (host->running) {
+		clk_enable(host->clk);
+		spdif_out_configure(host);
+		spdif_out_clock(host, host->saved_params.core_freq,
+				host->saved_params.rate);
+	}
+	return 0;
+}
+
+static SIMPLE_DEV_PM_OPS(spdif_out_dev_pm_ops, spdif_out_suspend, \
+		spdif_out_resume);
+
+#define SPDIF_OUT_DEV_PM_OPS (&spdif_out_dev_pm_ops)
+
+#else
+#define SPDIF_OUT_DEV_PM_OPS NULL
+
+#endif
+
 static struct platform_driver spdif_out_driver = {
 	.probe		= spdif_out_probe,
 	.remove		= spdif_out_remove,
 	.driver		= {
 		.name	= "spdif-out",
 		.owner	= THIS_MODULE,
+		.pm	= SPDIF_OUT_DEV_PM_OPS,
 	},
 };
 
