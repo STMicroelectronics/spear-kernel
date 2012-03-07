2df2f8b455a173e953fc182c62494cfc25ea977b
Viresh Kumar <viresh.kumar@st.com>
2012-03-07 10:59:34 +0530

    watchdog/mpcore_wdt: Arrange #includes in alphabetical order
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>

--- drivers/watchdog/mpcore_wdt.c
+++ drivers/watchdog/mpcore_wdt.c
@@ -20,16 +20,16 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/types.h>
-#include <linux/watchdog.h>
-#include <linux/reboot.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
-#include <linux/platform_device.h>
-#include <linux/slab.h>
 #include <linux/io.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/platform_device.h>
+#include <linux/reboot.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/watchdog.h>
 
 #include <asm/smp_twd.h>
 
