687e69cda173b27199f7120867cc8ce17af49043
Shiraz Hashim <shiraz.hashim@st.com>
2012-04-30 09:09:51 +0530

    smp_twd: reconfigure clockevents after cpufreq change
    This is a backport to linux-2.6.37, of the patch from 'Linux Walleij'
    
        commit 4fd7f9b128107034fa925b6877fae3c275f0da86
        Author: Linus Walleij <linus.walleij@linaro.org>
        Date:   Tue Dec 13 12:48:18 2011 +0100
    
            ARM: 7212/1: smp_twd: reconfigure clockevents after cpufreq change
    
            This break-out from Colin Cross' cpufreq-aware TWD patch
            will handle the case when our localtimer's clock changes with
            the cpu clock. A cpufreq transtion notifier will be registered
            only if the platform has supplied a specified clock to the TWD.
    
            After a cpufreq transition, update the clockevent's frequency
            by fetching the new clock rate from the clock framework and
            reprogram the next clock event.
    
            The necessary changes in the clockevents framework was done by
            Thomas Gleixner in kernel v3.0.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>

--- arch/arm/kernel/smp_twd.c
+++ arch/arm/kernel/smp_twd.c
@@ -11,6 +11,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/clk.h>
+#include <linux/cpufreq.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/err.h>
@@ -28,6 +29,8 @@ void __iomem *twd_base;
 
 static struct clk *twd_clk;
 static unsigned long twd_timer_rate;
+
+static struct clock_event_device __percpu **twd_evt;
 
 static void twd_set_mode(enum clock_event_mode mode,
 			struct clock_event_device *clk)
@@ -82,6 +85,52 @@ int twd_timer_ack(void)
 
 	return 0;
 }
+
+#ifdef CONFIG_CPU_FREQ
+
+/*
+ * Updates clockevent frequency when the cpu frequency changes.
+ * Called on the cpu that is changing frequency with interrupts disabled.
+ */
+static void twd_update_frequency(void *data)
+{
+	twd_timer_rate = clk_get_rate(twd_clk);
+
+	clockevents_update_freq(*__this_cpu_ptr(twd_evt), twd_timer_rate);
+}
+
+static int twd_cpufreq_transition(struct notifier_block *nb,
+	unsigned long state, void *data)
+{
+	struct cpufreq_freqs *freqs = data;
+
+	/*
+	 * The twd clock events must be reprogrammed to account for the new
+	 * frequency.  The timer is local to a cpu, so cross-call to the
+	 * changing cpu.
+	 */
+	if (state == CPUFREQ_POSTCHANGE || state == CPUFREQ_RESUMECHANGE)
+		smp_call_function_single(freqs->cpu, twd_update_frequency,
+			NULL, 1);
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block twd_cpufreq_nb = {
+	.notifier_call = twd_cpufreq_transition,
+};
+
+static int twd_cpufreq_init(void)
+{
+	if (!IS_ERR(twd_clk))
+		return cpufreq_register_notifier(&twd_cpufreq_nb,
+			CPUFREQ_TRANSITION_NOTIFIER);
+
+	return 0;
+}
+core_initcall(twd_cpufreq_init);
+
+#endif
 
 static void __cpuinit twd_calibrate_rate(void)
 {
@@ -149,6 +198,16 @@ static struct clk *twd_get_clock(void)
 void __cpuinit twd_timer_setup(struct clock_event_device *clk)
 {
 	unsigned long flags;
+	struct clock_event_device **this_cpu_clk;
+
+	if (!twd_evt) {
+
+		twd_evt = alloc_percpu(struct clock_event_device *);
+		if (!twd_evt) {
+			pr_err("twd: can't allocate memory\n");
+			return;
+		}
+	}
 
 	if (!twd_clk)
 		twd_clk = twd_get_clock();
@@ -164,6 +223,9 @@ void __cpuinit twd_timer_setup(struct clock_event_device *clk)
 	clk->rating = 350;
 	clk->set_mode = twd_set_mode;
 	clk->set_next_event = twd_set_next_event;
+
+	this_cpu_clk = __this_cpu_ptr(twd_evt);
+	*this_cpu_clk = clk;
 
 	clockevents_config_and_register(clk, twd_timer_rate,
 			0xf, 0xffffffff);
