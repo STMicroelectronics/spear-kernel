54b0b1b3781bea7b28eb0bd0b32641673c13d6fd
Deepak Sikri <deepak.sikri@st.com>
2012-05-15 12:15:54 +0530

    stmmac: Fix for nfs hang on multiple reboot
    It was observed that during multiple reboots nfs hangs. The status of
    receive descriptors shows that all the descriptors were in control of
    CPU, and none were assigned to DMA. Also the DMA status register
    confirmed that the Rx buffer is unavailable.  This patch adds the fix
    for the same by adding the memory barriers to ascertain that the all
    instructions before enabling the Rx or Tx DMA are completed which
    involves the proper setting of the ownership bit in DMA descriptors.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>

--- drivers/net/stmmac/stmmac_main.c
+++ drivers/net/stmmac/stmmac_main.c
@@ -1141,6 +1141,7 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 		priv->hw->desc->prepare_tx_desc(desc, 0, len, csum_insertion);
 		wmb();
 		priv->hw->desc->set_tx_owner(desc);
+		wmb();
 	}
 
 	/* Interrupt on completition only for the latest segment */
@@ -1156,6 +1157,7 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 
 	/* To avoid raise condition */
 	priv->hw->desc->set_tx_owner(first);
+	wmb();
 
 	priv->cur_tx++;
 
@@ -1216,6 +1218,7 @@ static inline void stmmac_rx_refill(struct stmmac_priv *priv)
 		}
 		wmb();
 		priv->hw->desc->set_rx_owner(p + entry);
+		wmb();
 	}
 }
 
