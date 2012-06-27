00c45e0c49ab0fa06bb0b1781aafab26fdad0932
Deepak Sikri <deepak.sikri@st.com>
2012-06-27 13:10:43 +0800

    ata/ahci_platform: Suspend to Ram/Disk support update
    This patch add in the plugins for suspend to disk. The suspend to ram  plugins
    have been updated to ensure that the port is suspended before the host.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>

--- drivers/ata/ahci_platform.c
+++ drivers/ata/ahci_platform.c
@@ -298,9 +298,13 @@ static int ahci_resume(struct device *dev)
 	return 0;
 }
 
-static struct dev_pm_ops ahci_pm_ops = {
-	.suspend		= &ahci_suspend,
-	.resume			= &ahci_resume,
+static const struct dev_pm_ops ahci_pm_ops = {
+	.suspend_noirq = ahci_suspend,
+	.resume_noirq = ahci_resume,
+	.freeze_noirq = ahci_suspend,
+	.thaw_noirq = ahci_resume,
+	.poweroff = ahci_suspend,
+	.restore = ahci_resume,
 };
 #endif
 
