c356079963b0a7ec44199bb80103eba337ee57b4
Viresh Kumar <viresh.kumar@st.com>
2012-06-27 13:10:41 +0800

    ata/ahci_platform: Add support for enabling/disabling clock.
    On many architectures, drivers are supposed to enable/disable functional clock
    of device. This patch adds support for clock enabling/disabling in
    ahci_platform.c.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>

--- drivers/ata/ahci.h
+++ drivers/ata/ahci.h
@@ -35,6 +35,7 @@
 #ifndef _AHCI_H
 #define _AHCI_H
 
+#include <linux/clk.h>
 #include <linux/libata.h>
 
 /* Enclosure Management Control */
@@ -302,6 +303,9 @@ struct ahci_host_priv {
 	u32 			em_loc; /* enclosure management location */
 	u32			em_buf_sz;	/* EM buffer size in byte */
 	u32			em_msg_type;	/* EM message type */
+#ifdef CONFIG_HAVE_CLK
+	struct clk *clk;	/* clk structure, only if HAVE_CLK is defined */
+#endif
 };
 
 extern int ahci_ignore_sss;
