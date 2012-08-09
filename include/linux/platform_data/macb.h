fb3f2a19c579ee3ca88b9bc098616df3729d9de8
Deepak Sikri <deepak.sikri@st.com>
2012-08-09 16:28:50 +0530

    net/macb: Add mdio clk control callback in platform declaration
    This patch adds in callback to select the mdio interface at run time
    based on the mac being used. This is very specific to SPEAr320 as the
    platform uses the muxed mdio lines to control the phy.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>

--- include/linux/platform_data/macb.h
+++ include/linux/platform_data/macb.h
@@ -7,11 +7,15 @@
  */
 #ifndef __MACB_PDATA_H__
 #define __MACB_PDATA_H__
+#include <linux/platform_device.h>
 
 struct macb_platform_data {
+	int		bus_id;
 	u32		phy_mask;
+	int		phy_addr;
 	int		phy_irq_pin;	/* PHY IRQ */
-	u8		is_rmii;	/* using RMII interface? */
+	u8              is_rmii;        /* using RMII interface? */
+	void		(*plat_mdio_control)(struct platform_device *);
 };
 
 #endif /* __MACB_PDATA_H__ */
