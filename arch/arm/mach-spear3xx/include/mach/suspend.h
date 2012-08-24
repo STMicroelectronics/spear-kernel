dfa8c3d3f0aaef1550597f707bbca01ce2a96f86
Deepak Sikri <deepak.sikri@st.com>
2012-08-24 13:38:27 +0530

    SPEAr3xx/6xx: Add macros to be used in PM code.
    This patch adds in the basic defines and macros to be used in the
    machine specific implementation of suspend to ram and standby for
    SPEAr6xx/3xx.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>

---
+++ arch/arm/mach-spear3xx/include/mach/suspend.h
@@ -0,0 +1,39 @@
+/*
+ * arch/arm/mach-spear3xx/include/mach/suspend.h
+ *
+ * Sleep mode defines for SPEAr3xx machine family
+ *
+ * Copyright (C) 2012 ST Microelectronics
+ * AUTHOR : Deepak Sikri <deepak.sikri@st.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __MACH_SUSPEND_H
+#define __MACH_SUSPEND_H
+
+#include <mach/spear.h>
+
+#ifndef __ASSEMBLER__
+extern void spear_sleep_mode(suspend_state_t state, unsigned long *saveblk);
+extern unsigned int spear_sleep_mode_sz;
+#endif
+
+/* SRAM Stack offsets */
+#define SRAM_STACK_STRT_OFF	0x650
+
+/* SRAM related defines*/
+#define SRAM_START_PA		SPEAR3XX_ICM1_SRAM_BASE
+#define SRAM_START_VA		VA_SPEAR3XX_ICM1_SRAM_BASE
+#define SRAM_SIZE		SZ_4K
+#define SRAM_STACK_PA		(SRAM_START_PA + SRAM_STACK_STRT_OFF)
+#define SRAM_STACK_VA		(SRAM_START_VA + SRAM_STACK_STRT_OFF)
+
+/* SPEAr subsystem physical addresses */
+#define SYS_CTRL_BASE_PA	SPEAR3XX_ICM3_SYS_CTRL_BASE
+#define MPMC_BASE_PA		SPEAR3XX_ICM3_SDRAM_CTRL_BASE
+#define MISC_BASE_PA		SPEAR3XX_ICM3_MISC_REG_BASE
+
+#endif /* __MACH_SUSPEND_H */
