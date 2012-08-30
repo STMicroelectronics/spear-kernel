488ef2425cfd8e5c74e272dc86ba520cc7560c61
Deepak Sikri <deepak.sikri@st.com>
2012-08-30 11:22:42 +0530

    ARM: Suspend to disk: Define dummy arch_prepare_suspend() function.
    The architectures supporting suspend to disk define
    arch_prepare_suspend() as a dummy function. This patch adds the same for
    arm architecture also.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>

--- arch/arm/include/asm/suspend.h
+++ arch/arm/include/asm/suspend.h
@@ -1,6 +1,8 @@
 #ifndef __ASM_ARM_SUSPEND_H
 #define __ASM_ARM_SUSPEND_H
 
+static inline int arch_prepare_suspend(void) { return 0; }
+
 extern void cpu_resume(void);
 extern int cpu_suspend(unsigned long, int (*)(unsigned long));
 
