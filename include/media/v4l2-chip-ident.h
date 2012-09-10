db04e59c692d2df591eccc3704a5c91452572caa
Bhavna Yadav <bhavna.yadav@st.com>
2012-09-10 09:45:06 +0530

    vs6725: Add vs6725 sensor driver
    This is a v4l2 sensor-level driver for the ST VS6725 camera.
    
    Signed-off-by: Bhavna Yadav <bhavna.yadav@st.com>

--- include/media/v4l2-chip-ident.h
+++ include/media/v4l2-chip-ident.h
@@ -146,6 +146,9 @@ enum {
 	/* module vs6624: just ident 6624 */
 	V4L2_IDENT_VS6624 = 6624,
 
+	/* module vs6725: just ident 6725 */
+	V4L2_IDENT_VS6725 = 6725,
+
 	/* module saa6752hs: reserved range 6750-6759 */
 	V4L2_IDENT_SAA6752HS = 6752,
 	V4L2_IDENT_SAA6752HS_AC3 = 6753,
