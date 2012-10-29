6e14c1fbb4afcc97ef86884fff8dea6c90024a5b
Vipin Kumar <vipin.kumar@st.com>
2012-10-29 12:07:30 +0530

    spear13xx: Register fsmc pnor flash clock
    Signed-off-by: Vipin Kumar <vipin.kumar@st.com>
    Reviewed-by: Shiraz Hashim <shiraz.hashim@st.com>

--- drivers/clk/spear/spear1310_clock.c
+++ drivers/clk/spear/spear1310_clock.c
@@ -724,6 +724,7 @@ void __init spear1310_clk_init(void)
 			SPEAR1310_PERIP1_CLK_ENB, SPEAR1310_FSMC_CLK_ENB, 0,
 			&_lock);
 	clk_register_clkdev(clk, NULL, "b0000000.nand");
+	clk_register_clkdev(clk, NULL, "a0000000.flash");
 
 	clk = clk_register_gate(NULL, "smi_clk", "ahb_clk", 0,
 			SPEAR1310_PERIP1_CLK_ENB, SPEAR1310_SMI_CLK_ENB, 0,
--- drivers/clk/spear/spear1340_clock.c
+++ drivers/clk/spear/spear1340_clock.c
@@ -819,6 +819,7 @@ void __init spear1340_clk_init(void)
 			SPEAR1340_PERIP1_CLK_ENB, SPEAR1340_FSMC_CLK_ENB, 0,
 			&_lock);
 	clk_register_clkdev(clk, NULL, "b0000000.nand");
+	clk_register_clkdev(clk, NULL, "a0000000.flash");
 
 	clk = clk_register_gate(NULL, "smi_clk", "ahb_clk", 0,
 			SPEAR1340_PERIP1_CLK_ENB, SPEAR1340_SMI_CLK_ENB, 0,
